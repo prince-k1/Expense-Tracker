57f516c52f46d69187f5fa3368681835e5b213a8
Prince Kumar Pandit <pp611223@gmail.com>
2025-09-07 23:51:24 +0530

    Make the backend api of the login

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -6,4 +6,22 @@ route.get('/', (req, res) => {
     res.render('login');
 })
 
+
+route.post('/',  async (req, res) => {
+    const {name, email, password} = req.body;
+    const e = await userTable.findOne({where: {email: email}});
+    console.log('emaia' ,e);
+    if(e){
+        if(e.dataValues.password == password){
+            res.send('User login Successfull');
+        }else{
+            res.send('User not authorized');
+        }
+    }else{
+        
+        res.send('User not found');
+    }
+})
+
+
 module.exports = route;
